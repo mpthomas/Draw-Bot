9a7fc48d7fc145d01d32174400b4c64f468702a6
Matthew Thomas <matt@matthews-macbook-air.local>
2017-09-30 23:08:59 -0400

    Initial Commit

---
+++ carto/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  carto
+//
+//  Created by Matthew Thomas on 9/30/17.
+//  Copyright © 2017 Matthew Thomas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
